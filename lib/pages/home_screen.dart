4d955873b8d241604bb61c1dfe3f9d6dbc0ce010
Lyes Tarzalt <lyes.tarzalt@scicom.com.my>
2023-12-21 12:06:55 +0800

    feat: wrap root with Material

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -60,41 +60,43 @@ class MainScreenState extends State<MainScreen> {
       theme: ThemeManager.lightTheme,
       darkTheme: ThemeManager.darkTheme,
       themeMode: _themeMode,
-      home: Scaffold(
-        body: PageTransitionSwitcher(
-          duration: const Duration(milliseconds: 400),
-          transitionBuilder: (
-            Widget child,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-          ) {
-            return FadeThroughTransition(
-              animation: animation,
-              secondaryAnimation: secondaryAnimation,
-              child: child,
-            );
-          },
-          child: _getPageWidget(_selectedIndex),
-        ),
-        bottomNavigationBar: NavigationBar(
-          height: 50,
-          indicatorColor: Colors.transparent,
-          selectedIndex: _selectedIndex,
-          onDestinationSelected: _onItemTapped,
-          destinations: const [
-            NavigationDestination(
-              icon: Icon(Icons.list),
-              label: 'Currencies',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.history),
-              label: 'History',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.settings),
-              label: 'Settings',
-            ),
-          ],
+      home: Material(
+        child: Scaffold(
+          body: PageTransitionSwitcher(
+            duration: const Duration(milliseconds: 400),
+            transitionBuilder: (
+              Widget child,
+              Animation<double> animation,
+              Animation<double> secondaryAnimation,
+            ) {
+              return FadeThroughTransition(
+                animation: animation,
+                secondaryAnimation: secondaryAnimation,
+                child: child,
+              );
+            },
+            child: _getPageWidget(_selectedIndex),
+          ),
+          bottomNavigationBar: NavigationBar(
+            height: 40,
+            indicatorColor: Colors.transparent,
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: _onItemTapped,
+            destinations: const [
+              NavigationDestination(
+                icon: Icon(Icons.list),
+                label: 'Currencies',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.history),
+                label: 'History',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -106,7 +108,7 @@ class MainScreenState extends State<MainScreen> {
     });
   }
 
-Widget _getPageWidget(int index) {
+  Widget _getPageWidget(int index) {
     Future<List<Currency>> completedFuture = Future.value(widget.currencies);
     switch (index) {
       case 0:
@@ -120,5 +122,4 @@ Widget _getPageWidget(int index) {
         return CurrencyListScreen(currenciesFuture: completedFuture);
     }
   }
-
 }
