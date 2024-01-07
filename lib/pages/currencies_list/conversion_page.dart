5fa4eb9eafbfb5293a0061bc041ce359b3d5230c
Lyes Tarzalt <lyestarzalt@gmail.com>
2024-01-07 19:08:15 +0800

    fix: force some widgets to be LTR

--- lib/pages/currencies_list/conversion_page.dart
+++ lib/pages/currencies_list/conversion_page.dart
@@ -6,6 +6,7 @@ import 'package:decimal/decimal.dart';
 import 'package:dinar_watch/widgets/conversion_rate_info.dart';
 import 'package:dinar_watch/widgets/flag_container.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'dart:ui' as ui;
 
 class CurrencyConverterPage extends StatefulWidget {
   final Currency currency; // The currency selected from the list
@@ -135,75 +136,78 @@ class CurrencyConverterPageState extends State<CurrencyConverterPage>
     return Scaffold(
       resizeToAvoidBottomInset: true,
       appBar: AppBar(
-        title:  Text(AppLocalizations.of(context)!.convert),
+        title: Text(AppLocalizations.of(context)!.convert),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(
-                height: screenHeight * 0.4,
-                child: Stack(
-                  children: [
-                    // Top Card - DZD input field
-                    AnimatedPositioned(
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.easeInOut,
-                      top: isDZDtoCurrency
-                          ? topCardTopPosition
-                          : bottomCardTopPosition,
-                      left: 16,
-                      right: 16,
-                      height: cardHeight,
-                      child: _buildCurrencyInput(
-                        isDZDtoCurrency ? amountController : resultController,
-                        'DZD',
-                        widget.currency.flag,
-                        isDZDtoCurrency
-                            ? amountFocusNode
-                            : resultFocusNode, // Pass the FocusNode
+      body: Directionality(
+        textDirection: ui.TextDirection.ltr,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: screenHeight * 0.4,
+                  child: Stack(
+                    children: [
+                      // Top Card - DZD input field
+                      AnimatedPositioned(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                        top: isDZDtoCurrency
+                            ? topCardTopPosition
+                            : bottomCardTopPosition,
+                        left: 16,
+                        right: 16,
+                        height: cardHeight,
+                        child: _buildCurrencyInput(
+                          isDZDtoCurrency ? amountController : resultController,
+                          'DZD',
+                          widget.currency.flag,
+                          isDZDtoCurrency
+                              ? amountFocusNode
+                              : resultFocusNode, // Pass the FocusNode
+                        ),
                       ),
-                    ),
-                    // Bottom Card - Foreign currency input field
-                    AnimatedPositioned(
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.easeInOut,
-                      top: isDZDtoCurrency
-                          ? bottomCardTopPosition
-                          : topCardTopPosition,
-                      left: 16,
-                      right: 16,
-                      height: cardHeight,
-                      child: _buildCurrencyInput(
-                        isDZDtoCurrency ? resultController : amountController,
-                        widget.currency.currencyCode,
-                        widget.currency.flag,
-                        isDZDtoCurrency
-                            ? resultFocusNode
-                            : amountFocusNode, // Pass the FocusNode
+                      // Bottom Card - Foreign currency input field
+                      AnimatedPositioned(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                        top: isDZDtoCurrency
+                            ? bottomCardTopPosition
+                            : topCardTopPosition,
+                        left: 16,
+                        right: 16,
+                        height: cardHeight,
+                        child: _buildCurrencyInput(
+                          isDZDtoCurrency ? resultController : amountController,
+                          widget.currency.currencyCode,
+                          widget.currency.flag,
+                          isDZDtoCurrency
+                              ? resultFocusNode
+                              : amountFocusNode, // Pass the FocusNode
+                        ),
                       ),
-                    ),
-                    // FAB positioned in the middle of the cards, aligned to the right
-                    Positioned(
-                      top: fabTopPosition,
-                      right: 8,
-                      child: FloatingActionButton(
-                        onPressed: _swapCurrencies,
-                        elevation: 2,
-                        child: const Icon(Icons.swap_vert),
+                      // FAB positioned in the middle of the cards, aligned to the right
+                      Positioned(
+                        top: fabTopPosition,
+                        right: 8,
+                        child: FloatingActionButton(
+                          onPressed: _swapCurrencies,
+                          elevation: 2,
+                          child: const Icon(Icons.swap_vert),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              ConversionRateInfo(
-                conversionRateText: _getConversionRateText(),
-                textStyle: ThemeManager.moneyNumberStyle(context),
-                backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
-              ),
-            ],
+                ConversionRateInfo(
+                  conversionRateText: _getConversionRateText(),
+                  textStyle: ThemeManager.moneyNumberStyle(context),
+                  backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -229,9 +233,7 @@ class CurrencyConverterPageState extends State<CurrencyConverterPage>
         borderRadius: (cardTheme.shape as RoundedRectangleBorder).borderRadius,
         side: BorderSide(
           color: borderColor,
-          width: focusNode.hasFocus
-              ? 2.0
-              : 1.0, 
+          width: focusNode.hasFocus ? 2.0 : 1.0,
         ),
       ),
       margin: cardTheme.margin,
@@ -239,12 +241,9 @@ class CurrencyConverterPageState extends State<CurrencyConverterPage>
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
         child: Row(
-          crossAxisAlignment:
-              CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            
-            if (currencyCode ==
-                'DZD') 
+            if (currencyCode == 'DZD')
               Image.asset('assets/dz.png', width: 50, height: 40)
             else if (flag!.isNotEmpty)
               FlagContainer(
@@ -258,14 +257,13 @@ class CurrencyConverterPageState extends State<CurrencyConverterPage>
               child: TextField(
                 focusNode: focusNode,
                 controller: controller,
-                decoration: ThemeManager.currencyInputDecoration(
-                    context, currencyCode),
+                decoration:
+                    ThemeManager.currencyInputDecoration(context, currencyCode),
                 style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
                     color: Theme.of(context).colorScheme.onSurface,
                     height: 0.0),
-
                 keyboardType: TextInputType.number,
                 textAlign: TextAlign.right,
                 enabled: isInputEnabled,
--- lib/widgets/currency_list_item.dart
+++ lib/widgets/currency_list_item.dart
@@ -5,6 +5,7 @@ import 'package:dinar_watch/theme/theme_manager.dart';
 import '../models/currency.dart';
 import 'package:dinar_watch/pages/currencies_list/conversion_page.dart';
 import 'package:dinar_watch/widgets/flag_container.dart';
+import 'dart:ui' as ui;
 
 class CurrencyListItem extends StatelessWidget {
   final Currency currency;
@@ -20,64 +21,69 @@ class CurrencyListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ListTile(
-          onTap: () {
-            // Navigate to CurrencyConverterPage
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => CurrencyConverterPage(currency: currency),
-              ),
-            );
-          },
-          leading:  FlagContainer(
-            imageUrl: currency.flag,
-            width: 50,
-            height: 40,
-            borderRadius: BorderRadius.circular(1),
-          ),
-          title: Hero(
-            tag: 'hero_currency_${currency.currencyCode}',
-            child: Material(
-              color: Colors.transparent,
-              child: Text(
-                currency.currencyCode,
-                style: ThemeManager.currencyCodeStyle(context),
+    return Directionality(
+      textDirection: ui.TextDirection.ltr,
+      child: Column(
+        children: [
+          ListTile(
+            onTap: () {
+              // Navigate to CurrencyConverterPage
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      CurrencyConverterPage(currency: currency),
+                ),
+              );
+            },
+            leading: FlagContainer(
+              imageUrl: currency.flag,
+              width: 50,
+              height: 40,
+              borderRadius: BorderRadius.circular(1),
+            ),
+            title: Hero(
+              tag: 'hero_currency_${currency.currencyCode}',
+              child: Material(
+                color: Colors.transparent,
+                child: Text(
+                  currency.currencyCode,
+                  style: ThemeManager.currencyCodeStyle(context),
+                ),
               ),
             ),
-          ),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(
-                width: 80, // Width of the container for the currency values
-                child: Text(
-                  _formatCurrencyValue(currency.buy), // Buy value
-                  style: ThemeManager.moneyNumberStyle(context),
-                  textAlign: TextAlign.left, // Align text to the left
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  width: 80, // Width of the container for the currency values
+                  child: Text(
+                    _formatCurrencyValue(currency.buy), // Buy value
+                    style: ThemeManager.moneyNumberStyle(context),
+                    textAlign: TextAlign.left, // Align text to the left
+                  ),
                 ),
-              ),
-              Text(
-                " DZD", // Currency unit
-                style: TextStyle(
-                  fontWeight: FontWeight.w300,
-                  fontSize: 12,
-                  color: Theme.of(context).colorScheme.onSurface,
+                Text(
+                  " DZD", // Currency unit
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 12,
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
                 ),
-              ),
-              const SizedBox(width: 15), // Space between DZD and arrow icon
-              Icon(
-                Icons.arrow_forward_ios,
-                color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
-                size: 16,
-              ),
-            ],
+                const SizedBox(width: 15), // Space between DZD and arrow icon
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color:
+                      Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
+                  size: 16,
+                ),
+              ],
+            ),
           ),
-        ),
-      const   Divider(height: 1)
-      ],
+          const Divider(height: 1)
+        ],
+      ),
     );
   }
 }
