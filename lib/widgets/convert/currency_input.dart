2c4e2a871069f7416ea6889ddc87322862dac5e0
Lyes Tarzalt <lyestarzalt@gmail.com>
2024-01-22 18:08:20 +0800

    fix: expneded in convert cuasing error

--- lib/widgets/convert/currency_input.dart
+++ lib/widgets/convert/currency_input.dart
@@ -12,71 +12,66 @@ Widget buildCurrencyInput({
 }) {
   bool isInputEnabled = controller == inputController;
 
-  return Expanded(
-    child: Card(
-      elevation: 1,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5),
-        side: BorderSide(
-          color: focusNode.hasFocus
-              ? Theme.of(context).colorScheme.onPrimaryContainer
-              : Theme.of(context)
-                  .colorScheme
-                  .onPrimaryContainer
-                  .withOpacity(0.3),
-          width: focusNode.hasFocus ? 3.0 : 0.5,
-        ),
+  return Card(
+    elevation: 1,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(5),
+      side: BorderSide(
+        color: focusNode.hasFocus
+            ? Theme.of(context).colorScheme.onPrimaryContainer
+            : Theme.of(context).colorScheme.onPrimaryContainer.withOpacity(0.3),
+        width: focusNode.hasFocus ? 3.0 : 0.5,
       ),
-      margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-      color: Theme.of(context).colorScheme.background,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 8.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            if (currencyCode == 'DZD')
-              Image.asset('assets/dz.png', width: 50, height: 40)
-            else if (flag!.isNotEmpty)
-              FlagContainer(
-                imageUrl: flag,
-                width: 50,
-                height: 40,
-                borderRadius: BorderRadius.circular(1),
-              ),
-            const SizedBox(width: 8.0),
-            Expanded(
-              child: TextField(
-                inputFormatters: [
-                  MaxNumberInputFormatter(1000000000000000),
-                  InputFormatter(context)
-                ],
-                focusNode: focusNode,
-                controller: controller,
-                decoration: InputDecoration(
-                  labelText: currencyCode.toUpperCase(),
-                  labelStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.onSurface,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w300),
-                  hintStyle: TextStyle(
-                    color: Theme.of(context).colorScheme.onSurface,
-                    fontSize: 16,
-                  ),
-                  border: InputBorder.none,
-                  contentPadding: const EdgeInsets.symmetric(vertical: 20.0),
-                ),
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).colorScheme.onSurface,
-                    height: 0.0),
-                keyboardType: TextInputType.number,
-                textAlign: TextAlign.right,
-                enabled: isInputEnabled,
-              ),
+    ),
+    margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+    color: Theme.of(context).colorScheme.background,
+    child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (currencyCode == 'DZD')
+            Image.asset('assets/dz.png', width: 50, height: 40)
+          else if (flag!.isNotEmpty)
+            FlagContainer(
+              imageUrl: flag,
+              width: 50,
+              height: 40,
+              borderRadius: BorderRadius.circular(1),
             ),
-          ],
-        ),
+          const SizedBox(width: 8.0),
+          Expanded(
+            child: TextField(
+              inputFormatters: [
+                MaxNumberInputFormatter(1000000000000000),
+                InputFormatter(context)
+              ],
+              focusNode: focusNode,
+              controller: controller,
+              decoration: InputDecoration(
+                labelText: currencyCode.toUpperCase(),
+                labelStyle: TextStyle(
+                    color: Theme.of(context).colorScheme.onSurface,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w300),
+                hintStyle: TextStyle(
+                  color: Theme.of(context).colorScheme.onSurface,
+                  fontSize: 16,
+                ),
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.symmetric(vertical: 20.0),
+              ),
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.onSurface,
+                  height: 0.0),
+              keyboardType: TextInputType.number,
+              textAlign: TextAlign.right,
+              enabled: isInputEnabled,
+            ),
+          ),
+        ],
       ),
     ),
   );
