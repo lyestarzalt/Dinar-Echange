2955fef1b9c455252dba633059245b7710abab79
Lyes Tarzalt <lyestarzalt@gmail.com>
2024-02-15 20:59:11 +0800

    feat: adaptive adbanner

--- lib/widgets/adbanner.dart
+++ lib/widgets/adbanner.dart
@@ -1,11 +1,10 @@
-import 'package:dinar_watch/providers/admob_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-
 import 'package:provider/provider.dart';
+import 'package:dinar_watch/providers/admob_provider.dart'; // Adjust the import based on your actual file structure
 
 class AdBannerWidget extends StatefulWidget {
-  const AdBannerWidget({super.key});
+  const AdBannerWidget({Key? key}) : super(key: key);
 
   @override
   _AdBannerWidgetState createState() => _AdBannerWidgetState();
@@ -18,22 +17,38 @@ class _AdBannerWidgetState extends State<AdBannerWidget> {
   @override
   void initState() {
     super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // This ensures we have a context with MediaQuery available
     _initBannerAd();
   }
 
-  void _initBannerAd() {
-    // Assuming AdProvider is accessible via Provider and it has been initialized properly.
+  void _initBannerAd() async {
     final adProvider = Provider.of<AdProvider>(context, listen: false);
+    final screenWidth = MediaQuery.of(context).size.width;
 
-    // You might want to adjust the ad size here or fetch it from the provider
-    BannerAd(
-      adUnitId: adProvider.bannerAdUnitId, // Your Ad Unit ID
-      size: AdSize.banner,
+    // Get adaptive banner size
+    final AdSize? adSize = await AdSize.getAnchoredAdaptiveBannerAdSize(
+      Orientation.portrait,
+      screenWidth.truncate(),
+    );
+
+    if (adSize == null) {
+      print("Unable to get adaptive banner size");
+      return;
+    }
+
+    final BannerAd bannerAd = BannerAd(
+      adUnitId: adProvider.bannerAdUnitId, // Use your ad unit ID
+      size: adSize,
       request: AdRequest(),
       listener: BannerAdListener(
         onAdLoaded: (Ad ad) {
           setState(() {
-            _bannerAd = ad as BannerAd?;
+            _bannerAd = ad as BannerAd;
             _isAdLoaded = true;
           });
         },
@@ -42,12 +57,19 @@ class _AdBannerWidgetState extends State<AdBannerWidget> {
           ad.dispose();
         },
       ),
-    ).load();
+    );
+
+    bannerAd.load();
   }
 
   @override
   Widget build(BuildContext context) {
-    return _isAdLoaded ? AdWidget(ad: _bannerAd!) : SizedBox();
+    return Container(
+      alignment: Alignment.center,
+      child: _isAdLoaded ? AdWidget(ad: _bannerAd!) : SizedBox(),
+      width: _bannerAd?.size.width.toDouble(),
+      height: _bannerAd?.size.height.toDouble(),
+    );
   }
 
   @override
