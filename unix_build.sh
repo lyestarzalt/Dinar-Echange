5bced804b35ea93e0bcd11993414f3d9e16946a7
Lyes Tarzalt <lyestarzalt@gmail.com>
2024-05-19 12:30:36 +0800

    temp fix for 3.22 gen-l10

--- unix_build.sh
+++ unix_build.sh
@@ -33,4 +33,9 @@ dart run arb_generator
 echo "Generating localized messages..."
 flutter gen-l10n
 
+# silly temp fix for 3.22
+dart run build_runner build --delete-conflicting-outputs
+flutter pub get
+dart run build_runner build --delete-conflicting-outputs
+# silly temp fix for 3.22
 echo "Build completed successfully!"
