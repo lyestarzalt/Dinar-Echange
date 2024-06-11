5eaebc1352a3b591378f44f984a6d009bc07d109
Lyes <lyes.trzlt@gmail.com>
2024-06-11 13:10:45 +0800

    fix: add back intl8 for currency list appbar

--- lib/views/currencies_list/main_view.dart
+++ lib/views/currencies_list/main_view.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:dinar_echange/data/models/currency.dart';
 import 'package:dinar_echange/views/currencies_list/list_currencies_view.dart';
 import 'package:provider/provider.dart';
+import 'package:dinar_echange/providers/app_provider.dart'; // Ensure you import the AppProvider
 import 'package:dinar_echange/providers/list_currency_provider.dart';
+import 'package:dinar_echange/l10n/gen_l10n/app_localizations.dart';
 
 class MainView extends StatefulWidget {
   final List<Currency> alternativeMarketCurrencies;
@@ -30,14 +32,30 @@ class _MainViewState extends State<MainView>
 
   @override
   Widget build(BuildContext context) {
+    final appProvider = Provider.of<AppProvider>(context,
+        listen: false); // Access the AppProvider
+    String formattedDate =
+        appProvider.getDatetime(widget.alternativeMarketCurrencies.first.date);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Currencies'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0, left: 16, top: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(formattedDate),
+              ],
+            ),
+          ),
+        ],
         bottom: TabBar(
           controller: _tabController,
           tabs: [
-            Tab(text: 'Alternative Market'),
-            Tab(text: 'Official Market'),
+            Tab(text: AppLocalizations.of(context)!.parallel_market),
+            Tab(text: AppLocalizations.of(context)!.official_market),
           ],
         ),
       ),
