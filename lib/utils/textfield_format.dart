020ecc3e7426825a38b2f470aa4691bca4779097
Lyes Tarzalt <lyestarzalt@gmail.com>
2024-12-15 11:14:29 +0800

    feat: enhance input formatter to handle empty input and limit decimal places

--- lib/utils/textfield_format.dart
+++ lib/utils/textfield_format.dart
@@ -3,33 +3,51 @@ import 'package:flutter/services.dart';
 class InputFormatter extends TextInputFormatter {
   static const double _maxValue = 1000000000000; // 1 trillion
 
+  static List<TextInputFormatter> getFormatters() {
+    return [
+      InputFormatter(),
+      FilteringTextInputFormatter.deny(RegExp('-')), // Prevent minus sign
+      FilteringTextInputFormatter.allow(
+          RegExp(r'[0-9.]')), // Only numbers and decimal
+    ];
+  }
+
   @override
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
+    // Handle empty input
     if (newValue.text.isEmpty) {
       return newValue;
     }
 
-    // Allow digits and a single decimal point
+    // Remove any non-digit and non-decimal characters
     String newText = newValue.text.replaceAll(RegExp(r'[^\d.]'), '');
     if (newText.isEmpty) {
-      return newValue;
+      return oldValue;
     }
 
-    // Check for more than one decimal point
+    // Handle multiple decimal points
     if (newText.indexOf('.') != newText.lastIndexOf('.')) {
       return oldValue;
     }
 
-    // Parsing the number to ensure it's within the limit
-    double value = double.tryParse(newText) ?? 0;
-    if (value > _maxValue) {
-      return oldValue; // If value exceeds the limit, revert to old value
+    // Prevent more than 2 decimal places
+    if (newText.contains('.')) {
+      List<String> parts = newText.split('.');
+      if (parts.length > 1 && parts[1].length > 2) {
+        return oldValue;
+      }
     }
 
-    return newValue.copyWith(
+    // Check value limits
+    double? value = double.tryParse(newText);
+    if (value == null || value > _maxValue) {
+      return oldValue;
+    }
+
+    return TextEditingValue(
       text: newText,
-      selection: newValue.selection, // Maintain the existing cursor position
+      selection: TextSelection.collapsed(offset: newText.length),
     );
   }
 }
